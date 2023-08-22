6cae544679c6e96e5975f8e4a9f95c831081f534
Francesco <francescomaisano@outlook.com.au>
2023-08-22 22:15:22 +0800

    Added menu functionality

--- src/App.js
+++ src/App.js
@@ -8,22 +8,30 @@ import {
   View,
   Card,
 } from "@aws-amplify/ui-react";
-
+import { useState } from "react"; // Import useState
 import { Menu, MenuItem } from '@aws-amplify/ui-react';
+import { SideBar } from './ui-components'; // Import your SideBar component
 
 function App({ signOut }) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false); // State to track sidebar open/close
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen); // Toggle sidebar state
+  };
+
   return (
     <View className="App">
       <View style={{ padding: '1rem' }}>
         <Menu>
-          <MenuItem>Option 1</MenuItem>
-          <MenuItem>Option 2</MenuItem>
-          <MenuItem>Option 3</MenuItem>
+          <MenuItem onClick={toggleSidebar}>Toggle Sidebar</MenuItem>
+          {/* Other menu items */}
         </Menu>
       </View>
       <View style={{ position: 'absolute', bottom: '1rem', left: '1rem' }}>
         <Button onClick={signOut}>Sign Out</Button>
       </View>
+      {/* Conditionally render Sidebar based on state */}
+      {isSidebarOpen && <SideBar />}
     </View>
   );
 }
